64bb3f29ddeeb61ab6896e8366f986b889d330fa
SeniorJack <45121264+ob-cell@users.noreply.github.com>
2022-04-20 03:39:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-// /index.js
-
-const server = require('server');
-const { get, socket } = server.router;
-const { render } = server.reply;
-
-server([
-  get('/', ctx => render('index.html'))
-]);
-const server = require('server');
-const { get, socket } = server.router;
-const { render } = server.reply;
-
-const updateCounter = ctx => {
-  ctx.io.emit('count', ctx.io.sockets.sockets.length);
-};
-
-server([
-  // For the initial load render the index.html
-  get('/', ctx => render('index.html')),
-
-  // Join/leave the room
-  socket('connect', updateCounter),
-  socket('disconnect', updateCounter)
-]);
-const server = require('server');
-const { get, socket } = server.router;
-const { render } = server.reply;
-
-// Update everyone with the current user count
-const updateCounter = ctx => {
-  ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
-};
-
-// Send the new message to everyone
-const sendMessage = ctx => {
-  ctx.io.emit('message', ctx.data);
-};
-
-server([
-  get('/', ctx => render('index.html')),
-  socket('connect', updateCounter),
-  socket('disconnect', updateCounter),
-  socket('message', sendMessage)
-]);
+(function() {
+    var chatBox = document.querySelector('.chat-box');
+    var inputBox = document.querySelector('.input-box');
+    var chatForm = document.querySelector('.chat-form');
+    
+    function addMessage(text) {
+        var newMessage = document.createElement('div');
+        newMessage.textContent = text;
+        chatBox.insertBefore(newMessage, chatBox.firstChild);
+    }
+    
+    chatForm.addEventListener('submit', function(event) {
+        if (inputBox.value.trim()) {
+            addMessage(inputBox.value);     
+            inputBox.value = '';            
+        }
+        event.preventDefault();
+    });
+})();
