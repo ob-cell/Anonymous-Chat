91835d58506185cd3c9792fe8905b496cb271e2d
SeniorJack <45121264+ob-cell@users.noreply.github.com>
2022-04-24 20:50:04 +0000

    javascript.js

--- index.js
+++ index.js
@@ -1,50 +1,15 @@
-// Create a reference to firebase
-  var messagesRef = new Firebase('https://hackeryou.firebaseio.com/chat');
+// /public/javascript.js
 
-  // C.R.E.A.M -  cache your elements
-  var messageField = $('#messageInput');
-  var nameField = $('#nameInput');
-  var messageList = $('.messages');
+// Get the current username from the cookies
+var user = cookie.get('user');
+if (!user) {
 
-  function addMessage(data) {
-    var username = data.name || 'anonymous';
-    var message = data.text;
-
-    // Create an element
-    var nameElement = $('<strong>').text(username);
-    var messageElement = $('<li>').text(message).prepend(nameElement);
-
-    // Add the message to the DOM
-    messageList.append(messageElement);
-
-    // Scroll to the bottom of the message list
-    messageList[0].scrollTop = messageList[0].scrollHeight;
+  // Ask for the username if there is none set already
+  user = prompt('Choose a username:');
+  if (!user) {
+    alert('We cannot work with you like that!');
+  } else {
+    // Store it in the cookies for future use
+    cookie.set('user', user);
   }
-
-  // Listen for the form submit
-  $('.chat').on('submit',function(e) {
-
-    // stop the form from submitting
-    e.preventDefault();
-
-    // create a message object
-    var message = {
-      name : nameField.val(),
-      text : messageField.val()
-    }
-
-    // Save Data to firebase
-    messagesRef.push(message);
-
-    // clear message field
-    messageField.val('');
-
-  });
-
-  // Add a callback that is triggered for each chat message
-  // this is kind of like an Ajax request, but they come in via websockets
-  // 10 of them will load on page load, and any future messages will as well
-  messagesRef.limitToLast(10).on('child_added', function (snapshot) {
-    // Get data from returned
-    addMessage(snapshot.val());
-  });
+}
