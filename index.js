e37c39ffb7066a55f0389d97f322653ab3f53f4b
SeniorJack <45121264+ob-cell@users.noreply.github.com>
2022-04-24 20:53:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -60,3 +60,46 @@ $('form').submit(function (e) {
   e.target.reset();
   $(e.target).find('input').focus();
 });
+const server = require('server');
+const { get, socket } = server.router;
+const { render } = server.reply;
+
+server([
+  get('/', ctx => render('index.html'))
+]);
+const server = require('server');
+const { get, socket } = server.router;
+const { render } = server.reply;
+
+const updateCounter = ctx => {
+  ctx.io.emit('count', ctx.io.sockets.sockets.length);
+};
+
+server([
+  // For the initial load render the index.html
+  get('/', ctx => render('index.html')),
+
+  // Join/leave the room
+  socket('connect', updateCounter),
+  socket('disconnect', updateCounter)
+]);
+const server = require('server');
+const { get, socket } = server.router;
+const { render } = server.reply;
+
+// Update everyone with the current user count
+const updateCounter = ctx => {
+  ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
+};
+
+// Send the new message to everyone
+const sendMessage = ctx => {
+  ctx.io.emit('message', ctx.data);
+};
+
+server([
+  get('/', ctx => render('index.html')),
+  socket('connect', updateCounter),
+  socket('disconnect', updateCounter),
+  socket('message', sendMessage)
+]);
